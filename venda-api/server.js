5cbd719336df00dc506e2aa6fed6af98c099e2b0
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2020-06-22 08:50:07 -0300

    Adiciona Dockerfile da aplicação Node Express

--- venda-api/server.js
+++ venda-api/server.js
@@ -1,13 +1,26 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get("/teste", (req, res) => {
+server.get('/validar-estoque', async (req, res) => {
+  const { produtoId, qtdDesejada } = req.query;
+  console.log(req.query);
+  await axios
+    .get(
+      `http://produto-api:8080/api/produtos/validar-estoque?id=${produtoId}&qtdDesejada=${qtdDesejada}`,
+    )
+    .then((data) => {
+      console.log(data.data);
+    })
+    .catch((error) => {
+      console.log(error.response.data.message);
+    });
   return res.json({
-    message: "Teste",
+    message: 'Teste',
   });
 });
 
