73fa45ffbc5da16b7b3293222d5dd3b0880af439
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2020-06-23 11:49:55 -0300

    Altera dados da documentação

--- venda-api/docs/apiDocumentation.js
+++ venda-api/docs/apiDocumentation.js
@@ -2,22 +2,12 @@ module.exports = {
   openapi: '3.0.1',
   info: {
     version: '1.3.0',
-    title: 'Users',
-    description: 'User management API',
-    termsOfService: 'http: //api_url/terms/',
-    contact: {
-      name: 'Wolox Team',
-      email: 'hello@wolox.co',
-      url: 'https: //www.wolox.com.ar/',
-    },
-    license: {
-      name: 'Apache 2.0',
-      url: 'https: //www.apache.org/licenses/LICENSE-2.0.html',
-    },
+    title: 'Venda-API',
+    description: 'Back-end do microsserviço Venda-API',
   },
   servers: [
     {
-      url: 'http: //localhost:3000/',
+      url: 'http: //localhost:8081/',
       description: 'Local server',
     },
   ],
