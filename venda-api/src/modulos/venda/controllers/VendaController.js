14d0bf04c64f7a2fc9e6179d841986acc263b56e
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2020-06-23 09:26:13 -0300

    Finaliza o CRUD de vendas

--- venda-api/src/modulos/venda/controllers/VendaController.js
+++ venda-api/src/modulos/venda/controllers/VendaController.js
@@ -14,8 +14,8 @@ class VendaController {
       }
       const vendaSalva = await Venda.create({
         usuarioId: authUser.id,
-        usuarioNome: authUser.id,
-        usuarioEmail: authUser.id,
+        usuarioNome: authUser.nome,
+        usuarioEmail: authUser.email,
         produtos,
         statusVenda: 'REALIZADA',
         dataVenda: new Date(),
@@ -28,19 +28,81 @@ class VendaController {
           reduzirEstoque: true,
         });
       });
-      console.log(produtosEstoque);
       mq.sendMessage(produtosEstoque);
       return res.json(vendaSalva);
     } catch (error) {
-      console.info(error);
       return res.status(400).json({ message: 'Não foi possível realizar a venda' });
     }
   }
 
-  async cancelarVenda(req, res) {}
-  async buscarTodasAsVendas(req, res) {}
-  async buscarVendaPorId(req, res) {}
-  async buscarVendaPorEmailUsuario(req, res) {}
-  async buscarPorNomeUsuario(req, res) {}
+  async cancelarVenda(req, res) {
+    const { id } = req.params;
+    try {
+      const venda = await Venda.findById(id);
+      if (!venda) {
+        return res.status(400).json({ message: 'A venda não foi encontrada' });
+      }
+      venda.statusVenda = 'CANCELADA';
+      await venda.save();
+      const produtosEstoque = [];
+      const produtos = venda.produtos;
+      produtos.forEach((produto) => {
+        produtosEstoque.push({
+          produtoId: produto.produtoId,
+          qtdDesejada: produto.qtdDesejada,
+          reduzirEstoque: false,
+        });
+      });
+      mq.sendMessage(produtosEstoque);
+      const vendaCancelada = await Venda.findById(id);
+      return res.json(vendaCancelada);
+    } catch (error) {
+      return res.status(400).json({ message: 'Não foi possível cancelar a venda' });
+    }
+  }
+  async buscarTodasAsVendas(req, res) {
+    try {
+      const vendas = await Venda.find();
+      return res.json(vendas);
+    } catch (error) {
+      return res.status(400).json({ message: 'Erro ao buscar as vendas.' });
+    }
+  }
+  async buscarVendaPorId(req, res) {
+    const { id } = req.params;
+    try {
+      const venda = await Venda.findById(id);
+      if (!venda) {
+        return res.status(400).json({ message: 'A venda não foi encontrada.' });
+      }
+      return res.json(venda);
+    } catch (error) {
+      return res.status(400).json({ message: 'Erro ao buscar venda.' });
+    }
+  }
+  async buscarVendaPorEmailUsuario(req, res) {
+    const { email } = req.params;
+    try {
+      const vendas = await Venda.find({ usuarioEmail: email });
+      if (!vendas) {
+        return res.status(400).json({ message: 'As vendas não foram encontradas.' });
+      }
+      return res.json(vendas);
+    } catch (error) {
+      return res.status(400).json({ message: 'Erro ao buscar vendas.' });
+    }
+  }
+  async buscarPorNomeUsuario(req, res) {
+    const { nome } = req.params;
+    try {
+      const vendas = await Venda.find({ usuarioNome: nome });
+      if (!vendas) {
+        return res.status(400).json({ message: 'As vendas não foram encontradas.' });
+      }
+      return res.json(vendas);
+    } catch (error) {
+      return res.status(400).json({ message: 'Erro ao buscar vendas.' });
+    }
+  }
 }
 export default new VendaController();
--- venda-api/src/modulos/venda/routes/index.js
+++ venda-api/src/modulos/venda/routes/index.js
@@ -1,17 +1,14 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import VendaController from "../controllers/VendaController";
+import VendaController from '../controllers/VendaController';
 
 const router = new Router();
 
-router.post("/api/vendas/nova-venda", VendaController.salvarVenda);
-router.put("/api/vendas/cancelar-venda/:id", VendaController.cancelarVenda);
-router.get("/api/vendas", VendaController.buscarTodasAsVendas);
-router.get("/api/vendas/:id", VendaController.buscarVendaPorId);
-router.get(
-  "/api/vendas/email/:email",
-  VendaController.buscarVendaPorEmailUsuario
-);
-router.get("/api/vendas/nome/:email", VendaController.buscarPorNomeUsuario);
+router.post('/api/vendas/nova-venda', VendaController.salvarVenda);
+router.put('/api/vendas/cancelar-venda/:id', VendaController.cancelarVenda);
+router.get('/api/vendas', VendaController.buscarTodasAsVendas);
+router.get('/api/vendas/:id', VendaController.buscarVendaPorId);
+router.get('/api/vendas/email/:email', VendaController.buscarVendaPorEmailUsuario);
+router.get('/api/vendas/nome/:nome', VendaController.buscarPorNomeUsuario);
 
 export default router;
