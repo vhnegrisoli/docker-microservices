319986dce3e9a94ee144b46fb1c46384353f3dcf
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-06-23 22:48:35 -0300

    Adiciona mais itens na documentação

--- venda-api/app.js
+++ venda-api/app.js
@@ -1,12 +1,12 @@
-import express, { Router } from 'express';
-import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import vendaRoutes from './src/modulos/venda/routes/index';
-import usuarioRoutes from './src/modulos/usuario/routes/index';
-import * as db from './src/config/db/config';
+import express, { Router } from "express";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import vendaRoutes from "./src/modulos/venda/routes/index";
+import usuarioRoutes from "./src/modulos/usuario/routes/index";
+import * as db from "./src/config/db/config";
 
-import checkToken from './src/config/auth/checkToken';
-import apiDocumentation from './docs/apiDocumentation';
+import checkToken from "./src/config/auth/checkToken";
+import apiDocumentation from "./docs/apiDocumentation";
 
 const app = express();
 
@@ -14,7 +14,7 @@ db.connect();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 app.use(usuarioRoutes);
 app.use(checkToken);
 app.use(vendaRoutes);
--- venda-api/docs/apiDocumentation.js
+++ venda-api/docs/apiDocumentation.js
@@ -1,14 +1,14 @@
 module.exports = {
-  openapi: '3.0.1',
+  openapi: "3.0.1",
   info: {
-    version: '1.3.0',
-    title: 'Venda-API',
-    description: 'Back-end do microsserviço Venda-API',
+    version: "1.3.0",
+    title: "Venda-API",
+    description: "Back-end do microsserviço Venda-API",
   },
   servers: [
     {
-      url: 'http: //localhost:8081/',
-      description: 'Local server',
+      url: "http: //localhost:8081/",
+      description: "Local server",
     },
   ],
   security: [
@@ -18,38 +18,126 @@ module.exports = {
   ],
   tags: [
     {
-      name: 'Usuários',
+      name: "Autenticação",
     },
     {
-      name: 'Vendas',
+      name: "Usuários",
+    },
+    {
+      name: "Vendas",
     },
   ],
   paths: {
-    '/api/usuarios': {
-      get: {
-        tags: ['Usuários'],
-        description: 'Buscar todos os usuários',
-        operationId: 'buscarTodosUsuarios',
+    "/auth/token": {
+      post: {
+        tags: ["Autenticação"],
+        description: "Autenticar usuários e devolver uma token JWT",
+        operationId: "auth",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/AuthRequest",
+              },
+            },
+          },
+        },
         responses: {
-          '200': {
-            description: 'Recupera todos os usuários',
+          "200": {
+            description: "Autenticar usuários e devolver uma token JWT",
             content: {
-              'application/json': {
+              "text/plain": {
                 schema: {
-                  $ref: '#/components/schemas/Usuarios',
+                  $ref: "#/components/schemas/AccessToken",
                 },
               },
             },
           },
-          '400': {
-            description: 'Erro ao buscar usuários',
+          "400": {
+            description: "Erro ao buscar usuários",
             content: {
-              'application/json': {
+              "application/json": {
                 schema: {
-                  $ref: '#/components/Error',
+                  $ref: "#/components/Error",
                 },
                 example: {
-                  message: 'Error message',
+                  message: "Error message",
+                  status: 400,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/auth/check_token": {
+      post: {
+        tags: ["Autenticação"],
+        description: "Verificar se o token de acesso é válido",
+        operationId: "checkToken",
+        parameters: [
+          {
+            in: "query",
+            name: "token",
+            description: "Token de acesso",
+            example:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoVXNlciI6eyJpZCI6IjVlZjJhYjg1NzEwODdhNDA5Y2E2NDA4OCIsImVtYWlsIjoidGVzdGVAZW1haWwuY29tIiwibm9tZSI6InRlc3RlIn0sImlhdCI6MTU5Mjk2MTk1MSwiZXhwIjoxNTkzMDQ4MzUxfQ.qQRi8z4HND78JDD8ApLVB6forT23moMGCR6VPsL0BQo",
+            required: true,
+          },
+        ],
+        responses: {
+          "200": {
+            description: "Validar se uma token é válida.",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/TokenAccepted",
+                },
+              },
+            },
+          },
+          "400": {
+            description: "Invalid parameters",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/Error",
+                },
+                example: {
+                  message: "Token inválida",
+                  internal_code: "invalid_parameters",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/usuarios": {
+      get: {
+        tags: ["Usuários"],
+        description: "Buscar todos os usuários",
+        operationId: "buscarTodosUsuarios",
+        responses: {
+          "200": {
+            description: "Recupera todos os usuários",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/Usuarios",
+                },
+              },
+            },
+          },
+          "400": {
+            description: "Erro ao buscar usuários",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/Error",
+                },
+                example: {
+                  message: "Error message",
                   status: 400,
                 },
               },
@@ -58,66 +146,40 @@ module.exports = {
         },
       },
       post: {
-        tags: ['Usuários'],
-        description: 'Create Usuarios',
-        operationId: 'createUsers',
+        tags: ["Usuários"],
+        description: "Salvar novo usuário",
+        operationId: "salvarUsuario",
         parameters: [],
         requestBody: {
           content: {
-            'application/json': {
+            "application/json": {
               schema: {
-                $ref: '#/components/schemas/Usuarios',
+                $ref: "#/components/schemas/Usuarios",
               },
             },
           },
           required: true,
         },
         responses: {
-          '200': {
-            description: 'New Usuarios were created',
-          },
-          '400': {
-            description: 'Invalid parameters',
+          "200": {
+            description: "Recupera todos os usuários",
             content: {
-              'application/json': {
+              "application/json": {
                 schema: {
-                  $ref: '#/components/Error',
-                },
-                example: {
-                  message: 'Usuario identificationNumbers 10, 20 already exist',
-                  internal_code: 'invalid_parameters',
+                  $ref: "#/components/schemas/Usuarios",
                 },
               },
             },
           },
-        },
-      },
-    },
-    '/api/vendas': {
-      get: {
-        tags: ['Vendas'],
-        description: 'Buscar todas as vendas',
-        operationId: 'buscarTodasAsVendas',
-        responses: {
-          '200': {
-            description: 'Recupera todas as vendas',
+          "400": {
+            description: "Invalid parameters",
             content: {
-              'application/json': {
+              "application/json": {
                 schema: {
-                  $ref: '#/components/schemas/Venda',
-                },
-              },
-            },
-          },
-          '400': {
-            description: 'Erro ao buscar vendas',
-            content: {
-              'application/json': {
-                schema: {
-                  $ref: '#/components/Error',
+                  $ref: "#/components/Error",
                 },
                 example: {
-                  message: 'Error message',
+                  message: "Erro ao salvar usuário",
                   status: 400,
                 },
               },
@@ -126,31 +188,64 @@ module.exports = {
         },
       },
     },
-    '/api/vendas/nova': {
+    "/api/vendas": {
+      get: {
+        tags: ["Vendas"],
+        description: "Buscar todas as vendas",
+        operationId: "buscarTodasAsVendas",
+        responses: {
+          "200": {
+            description: "Recupera todas as vendas",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/Venda",
+                },
+              },
+            },
+          },
+          "400": {
+            description: "Erro ao buscar vendas",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/Error",
+                },
+                example: {
+                  message: "Error message",
+                  status: 400,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/vendas/nova": {
       post: {
-        tags: ['Vendas'],
-        description: 'Buscar todas as vendas',
-        operationId: 'buscarTodasAsVendas',
+        tags: ["Vendas"],
+        description: "Salvar nova venda",
+        operationId: "salvarVenda",
         responses: {
-          '200': {
-            description: 'Recupera todas as vendas',
+          "200": {
+            description: "Salva uma nova venda",
             content: {
-              'application/json': {
+              "application/json": {
                 schema: {
-                  $ref: '#/components/schemas/Venda',
+                  $ref: "#/components/schemas/Venda",
                 },
               },
             },
           },
-          '400': {
-            description: 'Erro ao buscar vendas',
+          "400": {
+            description: "Erro ao buscar vendas",
             content: {
-              'application/json': {
+              "application/json": {
                 schema: {
-                  $ref: '#/components/Error',
+                  $ref: "#/components/Error",
                 },
                 example: {
-                  message: 'Error message',
+                  message: "Error message",
                   status: 400,
                 },
               },
@@ -159,31 +254,31 @@ module.exports = {
         },
       },
     },
-    '/api/vendas/cancelar/:id': {
+    "/api/vendas/cancelar/:id": {
       get: {
-        tags: ['Vendas'],
-        description: 'Buscar todas as vendas',
-        operationId: 'buscarTodasAsVendas',
+        tags: ["Vendas"],
+        description: "Buscar todas as vendas",
+        operationId: "buscarTodasAsVendas",
         responses: {
-          '200': {
-            description: 'Recupera todas as vendas',
+          "200": {
+            description: "Recupera todas as vendas",
             content: {
-              'application/json': {
+              "application/json": {
                 schema: {
-                  $ref: '#/components/schemas/Venda',
+                  $ref: "#/components/schemas/Venda",
                 },
               },
             },
           },
-          '400': {
-            description: 'Erro ao buscar vendas',
+          "400": {
+            description: "Erro ao buscar vendas",
             content: {
-              'application/json': {
+              "application/json": {
                 schema: {
-                  $ref: '#/components/Error',
+                  $ref: "#/components/Error",
                 },
                 example: {
-                  message: 'Error message',
+                  message: "Error message",
                   status: 400,
                 },
               },
@@ -196,101 +291,127 @@ module.exports = {
   components: {
     schemas: {
       Usuario: {
-        type: 'object',
+        type: "object",
         properties: {
           _id: {
-            type: 'string',
-            description: 'ID do usuário',
-            example: '5099803df3f4948bd2f98391',
+            type: "string",
+            description: "ID do usuário",
+            example: "5099803df3f4948bd2f98391",
           },
           nome: {
-            type: 'string',
-            example: 'João',
+            type: "string",
+            example: "João",
           },
           email: {
-            type: 'string',
-            example: 'joao@gmail.com',
+            type: "string",
+            example: "joao@gmail.com",
           },
           senha: {
-            type: 'string',
-            example: '123456',
+            type: "string",
+            example: "123456",
           },
         },
       },
       Venda: {
-        type: 'object',
+        type: "object",
         properties: {
           _id: {
-            type: 'string',
-            description: 'ID da venda',
-            example: '5099803df3f4948bd2f98391',
+            type: "string",
+            description: "ID da venda",
+            example: "5099803df3f4948bd2f98391",
           },
           produtos: {
-            type: 'array',
+            type: "array",
             items: {
-              $ref: '#/components/schemas/Produtos',
+              $ref: "#/components/schemas/Produtos",
             },
           },
           statusVenda: {
-            type: 'enum',
-            properties: ['REALIZADA', 'CANCELADA'],
-            example: 'REALIZADA',
+            type: "enum",
+            properties: ["REALIZADA", "CANCELADA"],
+            example: "REALIZADA",
           },
           usuarioId: {
-            type: 'string',
-            example: '5099803df3f4948bd2f98391',
+            type: "string",
+            example: "5099803df3f4948bd2f98391",
           },
           usuarioNome: {
-            type: 'string',
-            example: 'João',
+            type: "string",
+            example: "João",
           },
           usuarioEmail: {
-            type: 'string',
-            example: 'joao@gmail.com',
+            type: "string",
+            example: "joao@gmail.com",
           },
         },
       },
       Produtos: {
-        type: 'object',
+        type: "object",
         properties: {
           produtoId: {
-            type: 'integer',
-            description: 'ID do produto',
+            type: "integer",
+            description: "ID do produto",
             example: 1,
           },
           qtdDesejada: {
-            type: 'integer',
-            description: 'Quantidade desejada',
+            type: "integer",
+            description: "Quantidade desejada",
             example: 1,
           },
         },
       },
-      ProdutoResponse: {
-        
+      ProdutoResponse: {},
+      AuthRequest: {
+        type: "object",
+        properties: {
+          email: {
+            type: "string",
+            description: "Email do usuário",
+          },
+          senha: {
+            type: "string",
+            description: "Senha do usuário",
+          },
+        },
+      },
+      AccessToken: {
+        type: "object",
+        example:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoVXNlciI6eyJpZCI6IjVlZjJhYjg1NzEwODdhNDA5Y2E2NDA4OCIsImVtYWlsIjoidGVzdGVAZW1haWwuY29tIiwibm9tZSI6InRlc3RlIn0sImlhdCI6MTU5Mjk2MTk1MSwiZXhwIjoxNTkzMDQ4MzUxfQ.qQRi8z4HND78JDD8ApLVB6forT23moMGCR6VPsL0BQo",
+      },
+      TokenAccepted: {
+        type: "object",
+        properties: {
+          message: {
+            type: "string",
+            description: "Token autenticada.",
+            example: "Token autenticada.",
+          },
+        },
       },
       Usuarios: {
-        type: 'array',
+        type: "array",
         items: {
-          $ref: '#/components/schemas/Usuario',
+          $ref: "#/components/schemas/Usuario",
         },
       },
     },
     Error: {
-      type: 'object',
+      type: "object",
       properties: {
         message: {
-          type: 'string',
+          type: "string",
         },
-        internal_code: {
-          type: 'string',
+        status: {
+          type: "integer",
         },
       },
     },
     securitySchemes: {
       ApiKeyAuth: {
-        type: 'bearer token',
-        in: 'header',
-        name: 'Authorization',
+        type: "bearer token",
+        in: "header",
+        name: "Authorization",
       },
     },
   },
